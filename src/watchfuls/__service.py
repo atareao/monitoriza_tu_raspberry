056fcfdd726c4a677487dbd79e368c97b1f2715d
VSC55 <jpastor@cerebelum.net>
2019-05-10 16:15:30 +0200

    Crear __service.py como función general para comprobar el status de los servicios en systemd.
    Añadir control status para bind9, slapd, snmp, webmin.

---
+++ src/watchfuls/__service.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# Monitorize your Raspberry Pi
+#
+# Copyright © 2019  Javier Pastor (aka VSC55)
+# <jpastor at cerebelum dot net>
+#
+# Basado en nginxstatus.py de Lorenzo Carbonell (aka atareao)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import importlib
+
+def status(name_srv):
+    name_srv = name_srv.strip()
+    if not name_srv:
+        return False, 'Status Error: Service name is empty!!'
+
+    utils = importlib.import_module('__utils')
+    stdout, stderr = utils.execute('systemctl status '+name_srv)
+
+    if stdout == '':
+        return False, 'Status ['+name_srv+'] - Error:'+stderr[:-1]
+    return True, 'Status ['+name_srv+'] - Ok!'
---
+++ src/watchfuls/bind9status.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# Monitorize your Raspberry Pi
+#
+# Copyright © 2019  Javier Pastor (aka VSC55)
+# <jpastor at cerebelum dot net>
+#
+# Modulo basado en el codigo de Lorenzo Carbonell.
+#
+# Copyright © 2019  Lorenzo Carbonell (aka atareao)
+# <lorenzo.carbonell.cerezo at gmail dot com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import importlib
+
+class Watchful():
+    def __init__(self):
+        pass
+
+    def check(self):
+        service = importlib.import_module('__service')
+        return service.status("bind9")
+
+if __name__ == '__main__':
+    wf = Watchful()
+    print(wf.check())
