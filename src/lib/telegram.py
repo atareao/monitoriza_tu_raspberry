3a78100a09ed2c46df737032c00812c674ef31fa
Javier Pastor <jpastor@cerebelum.net>
2019-05-30 01:17:06 +0200

    Simplificar objeto telegram.py (mira que lo hacemos rebuscado en ocasiones :D)

--- src/lib/telegram.py
+++ src/lib/telegram.py
@@ -18,6 +18,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# http://www.forosdelweb.com/f130/resuelto-problema-con-hilos-malditos-hilos-python-xd-831605/
+
 
 import threading
 import requests
@@ -30,68 +33,33 @@ __all__ = ['Telegram']
 
 class Telegram(ObjectBase):
 
-    class PoolSendMsg(threading.Thread):
-
-        tg = None
-        __run_while = True
-        __group_messages = True
-
-        # https://rstopup.com/ejecutar-un-proceso-y-salir-sin-esperar-a-que-se.html
-        # def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, verbose=None):
-        #     super().__init__(self, group=group, target=target, name=name, verbose=verbose)
-        #     self.args = args
-        #     self.kwargs = kwargs
-        #     self.setDaemon(True)
-        #     self.tg = None
-        #     self.__run_while = True
-        #     return
-
-        def run(self):
-            self.__run_while = True
-            msg_group = ''
-            while True:
-                if self.tg and len(self.tg.list_msg) > 0:
-                    msg = self.tg.list_msg.pop()
-                    self.tg.debug.print("Telegram > Send > Msg: {0}".format(msg))
-                    if self.group_messages:
-                        msg_group += msg + "\n"
-                    else:
-                        self.tg.api_send_message(msg)
-                    self.tg.count_msg_send += 1
-                else:
-                    if self.group_messages:
-                        if msg_group:
-                            self.tg.api_send_message(msg_group)
-                            msg_group = ''
-
-                    if not self.__run_while:
-                        break
-            return
-
-        def stop(self):
-            self.__run_while = False
-
-        @property
-        def group_messages(self) -> bool:
-            return self.__group_messages
-
-        @group_messages.setter
-        def group_messages(self, val: bool):
-            self.__group_messages = val
-
     def __init__(self, token, chat_id):
         self.list_msg = []
         self.count_msg = 0
         self.count_msg_send = 0
         self.token = token
         self.chat_id = chat_id
+        self.group_messages = False
+        self.stop = False
+        self.reset_count()
+        self.__init_pool()
 
-        self.pool_send_msg = self.PoolSendMsg()
-        self.pool_send_msg.tg = self
-        self.pool_send_msg.setDaemon(True)
+    def __init_pool(self):
+        self.stop = False
+        self.pool_send_msg = threading.Thread(target=self.pool_run, daemon=True)
         self.pool_send_msg.start()
 
-        self.group_messages = False
+    @property
+    def group_messages(self) -> bool:
+        return self.__group_messages
+
+    @group_messages.setter
+    def group_messages(self, val: bool):
+        self.__group_messages = val
+
+    def reset_count(self):
+        self.count_msg = 0
+        self.count_msg_send = 0
 
     def send_message(self, message):
         self.add_list(message)
@@ -99,7 +67,6 @@ class Telegram(ObjectBase):
     def send_message_end(self, hostname):
         if self.count_msg > 0:
             s_message = "Summary *{0}*, get *{1}* new Message.".format(hostname, self.count_msg)
-            # s_message = "{0} {1} {2} {3}{3}{3}".format(u'\U00002139', u'\U0001F4BB', s_message, u'\U0000261D')
             s_message = "{0} {1} {2}{2}{2}".format(u'\U00002139', s_message, u'\U0000261D')
             self.add_list(s_message)
             # Sleep para asegurarnos de que el mensaje anterior esta en la lista antes de iniciar el siguiente While.
@@ -107,16 +74,43 @@ class Telegram(ObjectBase):
 
         # Esperamos a que la lista de mensajes esta vacía.
         while True:
-            if len(self.list_msg) == 0:
+            if self.is_entry_list:
                 break
 
-        self.count_msg = 0
-        self.count_msg_send = 0
+        self.reset_count()
+
+    @property
+    def is_entry_list(self) -> bool:
+        if len(self.list_msg) == 0:
+            return True
+        return False
 
     def add_list(self, message):
         # Efectuamos insert para mantener el orden.
         self.list_msg.insert(0, message)
         self.count_msg += 1
+
+    def pool_run(self):
+        self.stop = False
+        msg_group = ''
+        while True:
+            if not self.is_entry_list:
+                msg = self.list_msg.pop()
+                self.debug.print("Telegram > Send > Msg: {0}".format(msg))
+                if self.group_messages:
+                    msg_group += msg + "\n"
+                else:
+                    self.api_send_message(msg)
+                self.count_msg_send += 1
+            else:
+                if self.group_messages:
+                    if msg_group:
+                        self.api_send_message(msg_group)
+                        msg_group = ''
+                else:
+                    if self.stop:
+                        break
+        return
 
     def api_send_message(self, message):
         if message and self.token and self.chat_id:
@@ -128,14 +122,5 @@ class Telegram(ObjectBase):
         if not self.chat_id:
             self.debug.print("Error: Telegram Chat ID is Null", DebugLevel.error)
         return False
-
-    @property
-    def group_messages(self) -> bool:
-        return self.pool_send_msg.group_messages
-
-    @group_messages.setter
-    def group_messages(self, val: bool):
-        self.pool_send_msg.group_messages = val
-
 
 # https://apps.timwhitlock.info/emoji/tables/unicode
