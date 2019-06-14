838cf53eee51a2ed143282f2de11187877597f83
Javier Pastor <jpastor@cerebelum.net>
2019-06-14 20:49:29 +0200

    Nuevo plugin para detectar status de los volúmenes RAID.

---
+++ src/watchfuls/raid.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Monitorize your Raspberry Pi
+#
+# Copyright © 2019  Javier Pastor (aka vsc55)
+# <jpastor at cerebelum dot net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib import Switch
+from lib.modules import ModuleBase
+from lib.linux import RAID_Mdstat
+
+
+class Watchful(ModuleBase):
+
+    __path_mdstat = "/proc/mdstat"
+
+    def __init__(self, monitor):
+        super().__init__(monitor, __name__)
+
+    def check(self):
+        list_md = RAID_Mdstat(self.__path_mdstat).read_status()
+        if len(list_md) == 0:
+            message = "*No RAID's* in the system. {0}".format(u'\U00002705')
+            self.dict_return.set("None", True, message)
+        else:
+            for (key, value) in list_md.items():
+                # print("key:", key, " - Val:", value)
+
+                is_warning = True
+                with Switch(value.get("update", '')) as case:
+                    if case(RAID_Mdstat.UpdateStatus.ok):
+                        is_warning = False
+                        message = "RAID *{0}* in good status. {1}".format(key, u'\U00002705')
+
+                    elif case(RAID_Mdstat.UpdateStatus.error):
+                        message = "*RAID {0} is degraded.* {1}".format(key, u'\U000026A0')
+
+                    elif case(RAID_Mdstat.UpdateStatus.recovery):
+                        message = "*RAID {0} is degraded, recovery status {1}%, estimate time to finish {2}.* {3}".\
+                            format(key,
+                                   value.get("recovery", {}).get('percent', -1),
+                                   value.get("recovery", {}).get('finish', -1),
+                                   u'\U000026A0'
+                                   )
+
+                    else:
+                        message = "*RAID {0} Unknown Error*. {1}".format(key, u'\U000026A0')
+
+                other_data = {}
+                self.dict_return.set(key, not is_warning, message, other_data=other_data)
+
+        super().check()
+        return self.dict_return
